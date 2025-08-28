bb77d3715efb8809849089609b185fc862aaebf6
lavish51b1 <lavishgoyal0209@gmail.com>
2025-08-28 15:42:15 +0530

    j dhgddbj

--- scripts/array-access.sh
+++ scripts/array-access.sh
@@ -4,8 +4,8 @@ fruits=("Mango" "pineapple" "kiwi")
 
 
 
-echo "first fruit: ${fruits[0]}"
+echo "first fruit: ${fruits[0]}"  #first fruit: Mango
 
-echo "second fruit: ${fruits[1]}"
+echo "second fruit: ${fruits[1]}" #second fruit: pineapple
 
-echo "all fruits: ${fruits[@]}"
+echo "all fruits: ${fruits[@]}" #all fruits: Mango pinapple kiwi
